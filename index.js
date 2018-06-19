db9c0b2b7d4fb3c5fbd6773e34b74e49fa55aaab
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-06-19 22:32:11 +0100

    made few changes

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require ('express');
 
 const app = express();
-//const router = express.Router();
-//const request = require ('request');
 
 app.get('/', (req, res) =>{
-  res.status('success');
+  res.status(200).json('success');
 });
 
-app.post('/data',(req, res)=>{
-  req('https:we are good', (req, res, body) =>{
-  }).then(data=> res.json(data)).catch(err=> res.status(500).json('error creating API'));
+app.post('/data',(req, res) =>{
+  let { home , address } = req.body;
+  data.create({
+    home,
+    address
+  
+}).then(data=> res.json(data)).catch(err=> res.status(500).json('error creating API'));
 });
 
-app.get('/data', (req, res)=>{
+app.get('/data', (req, res) =>{
  res.data('Any string man');
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () =>{
   console.log('we are live on port 3000')
 })
