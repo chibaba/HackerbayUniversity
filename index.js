cef4e3b1043c5f691dcf536edf85894990a56abf
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-07-01 22:36:55 +0100

    modified

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/', (req, res) => {
 app.post('/data',(req, res) => {
   let post = req.body.data
   data.push(post)
-}).then(data=> res.json(post)).catch(err=> res.status(500).json('error creating API'));
+.then(data=> res.json(post)).catch(err=> res.status(500).json('error creating API'));
+});
 
 app.get('/data', (req, res) => {
  res.json(data);
